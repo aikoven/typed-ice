919b3fb9e030c9e87fb31a6a5d4c7434401f5012
Daniel Lytkin <gaelco.salmon@gmail.com>
2016-12-20 15:31:19 +0700

    Add HashMap constructors and methods

--- Ice/HashMap.d.ts
+++ Ice/HashMap.d.ts
@@ -3,11 +3,16 @@ import {Ice} from './Ice';
 declare module './Ice' {
   namespace Ice {
     export class HashMap<K, V> {
+      constructor();
+      constructor(from: HashMap<K, V>);
+
       forEach(fn: (key: K, value: V) => void, obj?: any): void;
 
       get(key: K): V | undefined;
       set(key: K, value: V): void;
       has(key: K): boolean;
+      clone(): HashMap<K, V>;
+      merge(from: HashMap<K, V>): void;
     }
   }
 }
