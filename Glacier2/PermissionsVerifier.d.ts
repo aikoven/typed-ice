26355b81cf0e22a5fb83a70e46a27e2b956f4148
Daniel Lytkin <gaelco.salmon@gmail.com>
2017-01-30 12:37:30 +0700

    Update typings for exceptions

--- Glacier2/PermissionsVerifier.d.ts
+++ Glacier2/PermissionsVerifier.d.ts
@@ -4,6 +4,8 @@ declare module './Glacier2' {
   namespace Glacier2 {
     export class PermissionDeniedException extends Ice.UserException {
       reason: string;
+
+      constructor(reason?: string, cause?: any);
     }
 
     export class PermissionsVerifier extends Ice.Object {
--- Ice/Exception.d.ts
+++ Ice/Exception.d.ts
@@ -4,6 +4,8 @@ declare module './Ice' {
   namespace Ice {
     export class Exception extends Error {
       constructor(cause?: any);
+
+      ice_cause: any;
       
       ice_name(): string;
     }
