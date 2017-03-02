b57aea6f998bce87c1005565fa32112b9f0dfa3a
Daniel Lytkin <gaelco.salmon@gmail.com>
2017-03-02 18:37:19 +0700

    Add `Ice.Properties.getPropertyWithDefault()`

--- Ice/Properties.d.ts
+++ Ice/Properties.d.ts
@@ -7,6 +7,7 @@ declare module './Ice' {
       constructor(args?: string[], defaults?: Properties);
 
       getProperty(key: string): string;
+      getPropertyWithDefault(key: string, value: string): string;
       setProperty(key: string, value: string): void;
       parse(data: string): void;
       parseCommandLineOptions(prefix: string, options: string[]): string[];
