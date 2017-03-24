b137c9e2239d549546c75e6b14f69ae14c7bf68d
Daniel Lytkin <gaelco.salmon@gmail.com>
2017-03-24 17:57:18 +0700

    Update ObjectAdapter typings

--- Ice/ObjectAdapter.d.ts
+++ Ice/ObjectAdapter.d.ts
@@ -5,7 +5,13 @@ declare module './Ice' {
     export interface ObjectAdapter {
       getCommunicator(): Ice.Communicator;
       add(servant: Ice.Object, id: Ice.Identity): Ice.ObjectPrx;
+      addFacet(servant: Ice.Object, id: Ice.Identity, facet: string): Ice.ObjectPrx;
+      addWithUUID(servant: Ice.Object): Ice.ObjectPrx;
+      addFacetWithUUID(servant: Ice.Object, facet: string): Ice.ObjectPrx;
+      addDefaultServant(servant: Ice.Object, category: string): void;
       remove(id: Ice.Identity): Ice.Object;
+      removeFacet(id: Ice.Identity, facet: string): Ice.Object;
+      removeDefaultServant(category: string): Ice.Object;
       destroy(): Ice.Promise<void>;
     }
   }
