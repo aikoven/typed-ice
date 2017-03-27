02021a32b0e7e6a494b3e246a50f6831b2ab054d
Daniel Lytkin <gaelco.salmon@gmail.com>
2017-03-27 16:44:39 +0700

    Update ObjectAdapter typings

--- Ice/ObjectAdapter.d.ts
+++ Ice/ObjectAdapter.d.ts
@@ -4,14 +4,21 @@ declare module './Ice' {
   namespace Ice {
     export interface ObjectAdapter {
       getCommunicator(): Ice.Communicator;
+
       add(servant: Ice.Object, id: Ice.Identity): Ice.ObjectPrx;
       addFacet(servant: Ice.Object, id: Ice.Identity, facet: string): Ice.ObjectPrx;
       addWithUUID(servant: Ice.Object): Ice.ObjectPrx;
       addFacetWithUUID(servant: Ice.Object, facet: string): Ice.ObjectPrx;
       addDefaultServant(servant: Ice.Object, category: string): void;
+
       remove(id: Ice.Identity): Ice.Object;
       removeFacet(id: Ice.Identity, facet: string): Ice.Object;
       removeDefaultServant(category: string): Ice.Object;
+
+      createProxy(id: Ice.Identity): Ice.ObjectPrx;
+      createDirectProxy(id: Ice.Identity): Ice.ObjectPrx;
+      createIndirectProxy(id: Ice.Identity): Ice.ObjectPrx;
+
       destroy(): Ice.Promise<void>;
     }
   }
