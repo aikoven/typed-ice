917d2c4823ac365a20a8f32180554a1c6675fb66
Daniel Lytkin <gaelco.salmon@gmail.com>
2017-03-27 16:56:45 +0700

    Update Local Exceptions typings

--- Ice/LocalException.d.ts
+++ Ice/LocalException.d.ts
@@ -2,6 +2,36 @@ import {Ice} from './Ice';
 
 declare module './Ice' {
   namespace Ice {
-    export class ConnectFailedException extends Ice.LocalException {}
+    export class RequestFailedException extends Ice.LocalException {
+      id: Ice.Identity;
+      facet: string;
+      operation: string;
+
+      constructor(id?: Ice.Identity, facet?: string, operation?: string,
+                  cause?: any);
+    }
+
+    export class ObjectNotExistException extends RequestFailedException {}
+
+    export class FacetNotExistException extends RequestFailedException {}
+
+    export class OperationNotExistException extends RequestFailedException {}
+
+
+    export class SyscallException extends Ice.LocalException {
+      error: number;
+
+      constructor(error?: number, cause?: any);
+    }
+
+    export class SocketException extends SyscallException {}
+
+    export class ConnectFailedException extends SocketException {}
+
+    export class ConnectionRefusedException extends ConnectFailedException {}
+
+    export class ConnectionLostException extends SocketException {}
+
+
   }
 }
