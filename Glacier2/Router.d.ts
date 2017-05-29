d1ac76180862c4ab44ee11ae85283717f5dd6671
Daniel Lytkin <gaelco.salmon@gmail.com>
2017-05-29 15:29:57 +0700

    Update `Glacier2.Router`

--- Glacier2/Router.d.ts
+++ Glacier2/Router.d.ts
@@ -2,15 +2,17 @@ import {Ice} from '../Ice/Ice';
 
 declare module './Glacier2' {
   namespace Glacier2 {
-    export interface RouterPrx extends Ice.RouterPrx {
-      createSession(userId: string, password: string,
-                    context?: Ice.Context):
-        Ice.Promise<Glacier2.SessionPrx>;
-      destroySession(): Ice.Promise<void>;
-      getACMTimeout(): Ice.Promise<number>;
-      getCategoryForClient(): Ice.Promise<string>;
-    }
+    export class SessionNotExistException extends Ice.UserException {}
 
+    export interface RouterPrx extends Ice.RouterPrx {
+      getCategoryForClient(context?: Ice.Context): Ice.Promise<string>;
+      createSession(userId: string, password: string,
+                    context?: Ice.Context): Ice.Promise<Glacier2.SessionPrx>;
+      refreshSession(context?: Ice.Context): Ice.Promise<void>;
+      destroySession(context?: Ice.Context): Ice.Promise<void>;
+      getSessionTimeout(context?: Ice.Context): Ice.Promise<Ice.Long>;
+      getACMTimeout(context?: Ice.Context): Ice.Promise<number>;
+    }
     export const RouterPrx: Ice.ObjectPrxStatic<RouterPrx>;
   }
 }
