cbe9ed5d5f6085f7522e92bea0d508003946429c
Daniel Lytkin <gaelco.salmon@gmail.com>
2017-06-26 14:50:14 +0700

    Add `HashMap.size` and `HashMap.entries`

--- Ice/HashMap.d.ts
+++ Ice/HashMap.d.ts
@@ -2,9 +2,18 @@ import './Ice';
 
 declare module './Ice' {
   namespace Ice {
+    interface HashMapEntry<K, V> {
+      key: K;
+      value: V;
+      next: HashMapEntry<K, V> | null;
+    }
+
     export class HashMap<K, V> {
       constructor();
       constructor(from: HashMap<K, V>);
+
+      readonly size: number;
+      readonly entries: HashMapEntry<K, V> | null;
 
       forEach(fn: (key: K, value: V) => void, obj?: any): void;
       keys(): K[];
