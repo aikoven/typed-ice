08594720ea64a321e06578c546dcc691a03138ef
egor <eg0r.n1k0l43v@gmail.com>
2017-11-30 13:44:39 +0700

    fix IceGrid.QueryPrx typings

--- IceGrid/Query.d.ts
+++ IceGrid/Query.d.ts
@@ -7,15 +7,15 @@ export {IceGrid}
 declare module "./IceGrid" {
   namespace IceGrid {
     export interface QueryPrx extends Ice.ObjectPrx {
-      findObjectById(id: Ice.Identity): Ice.ObjectPrx | null;
-      findObjectByType(type: string): Ice.ObjectPrx | null;
+      findObjectById(id: Ice.Identity): Ice.Promise<Ice.ObjectPrx | null>;
+      findObjectByType(type: string): Ice.Promise<Ice.ObjectPrx | null>;
       /**
        * todo: implements IceGrid.LoadSample
        * findObjectByTypeOnLeastLoadedNode(
        *    type: string, sample: IceGrid.LoadSample): Ice.ObjectPrx | null;
        * */
-      findAllObjectsByType(type: string): Ice.ObjectPrx[];
-      findAllReplicas(proxy: Ice.ObjectPrx): Ice.ObjectPrx[];
+      findAllObjectsByType(type: string): Ice.Promise<Ice.ObjectPrx[]>;
+      findAllReplicas(proxy: Ice.ObjectPrx): Ice.Promise<Ice.ObjectPrx[]>;
     }
     export const QueryPrx: Ice.ObjectPrxStatic<QueryPrx>;
   }
