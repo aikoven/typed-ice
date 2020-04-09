f6446ae479c8c83a675eac292e0f9f56594c4047
aikoven <dan.lytkin@gmail.com>
2020-04-09 12:05:09 +0700

    Add ValueConstructor type

--- src/Ice/Value.d.ts
+++ src/Ice/Value.d.ts
@@ -2,6 +2,11 @@ export {Ice} from './Ice.ns';
 
 declare module './Ice.ns' {
   namespace Ice {
+    interface ValueConstructor<T extends Value> {
+      new (...args: any[]): T;
+      ice_staticId(): string;
+    }
+
     class Value {
       static ice_staticId(): string;
 
